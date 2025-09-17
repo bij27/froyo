071ce40b991a8a04926d0396db9ea1e2614ce2ea
Bijan Jadhavji <12b.jad@gmail.com>
2025-09-16 21:04:24 -0500

    created loop and flavor count. if loop finds same flavor, it adds to the count. if different, it moves count to a different variable.

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ let flavorsArray = userInput.split(",");
 function getTotalFlavors(flavorsArray) {
   const flavorCounts = {};
 
-  
+  for (let i = 0; i < flavorsArray.length; i++) {
+    const flavor = flavorsArray[i];
+
+    if (flavorCounts[flavor]) {
+      flavorCounts[flavor]++;
+    } else {
+      flavorCounts[flavor] = 1;
+    }
+  }
+  return flavorCounts;
+}
+
